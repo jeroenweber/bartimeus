58d45c2bae8499b0e9b1724489f457245efbbcf0
jeroenweber <jeroen.weber@hu.nl>
2021-11-19 11:12:35 +0100

    First addition

--- repository/db_controller.py
+++ repository/db_controller.py
@@ -1,20 +1,32 @@
 import mysql.connector
 
+global mydb
+global mycursor
+
 def dbconnect():
+  global mydb, mycursor
   mydb = mysql.connector.connect(host='145.89.192.95', user='dbuser', passwd='Dbuser123!', database='ontime')
-  return mydb
+  mycursor = mydb.cursor()
+
+def dbclose():
+  global mydb, mycursor
+  mycursor.close()
+  mydb.close()
 
 def getpeople():
+  global mycursor
   people = []
   sql = 'SELECT * FROM people;'
-  mydb = dbconnect()
-  mycursor = mydb.cursor()
   mycursor.execute(sql)
   for (record) in mycursor:
     people.append(record)
-  mycursor.close()
-  mydb.close()
   return people
 
-
-
+def executequery(query):
+  dbconnect()
+  if (query == 'people'):
+    resultset = getpeople()
+  else:
+    resultset = ''
+  dbclose()
+  return resultset
