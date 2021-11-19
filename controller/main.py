6c4d9d448661d74846ea09340576c5cd4fcaf170
jeroenweber <jeroen.weber@hu.nl>
2021-11-19 10:33:08 +0100

    First addition

---
+++ controller/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from repository import db_controller
+
+app = Flask(__name__)
+
+@app.route('/people', methods=["GET"])
+def get_people():
+    people = db_controller.getpeople()
+    return jsonify(people)
+
+"""
+Enable CORS. Disable it if you don't need CORS
+"""
+@app.after_request
+def after_request(response):
+    response.headers["Access-Control-Allow-Origin"] = "*" # <- You can change "*" for a domain for example "http://localhost"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
+    return response
+
+
+if __name__ == "__main__":
+    app.run()
